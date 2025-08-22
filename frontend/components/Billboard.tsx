2d9492060d200ad1283b4864da7d2ce5122ea81f
haus0409 <haconghau07@gmail.com>
2025-08-23 00:35:24 +0700

    update giao dien

--- frontend/components/Billboard.tsx
+++ frontend/components/Billboard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const Billboard = () => {
     const data = {
@@ -15,7 +14,54 @@ const Billboard = () => {
     }
     return (
         <div className="relative h-[56.26vw]">
-            <video poster={data.thumbnailUrl} src={data.videoUrl}></video>
+            <video 
+                className="w-full
+                           h-[56.25vw]
+                           object-cover
+                           brightness-[60%]"       
+                autoPlay
+                muted
+                loop
+                poster={data.thumbnailUrl}
+                src={data.videoUrl}>
+            </video>
+            <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+                <p className="text-white text-1xl md:text-5xl h-fullw-[50%] lg:text-6xl font-bold drop-shadow-xl">
+                    {data.title }
+                </p>
+                <p className="text-white
+                                text-[8px]
+                                md:text-lg
+                                mt-3
+                                w-[80%]
+                                md:w-[80%]
+                                lg:w-[50%]
+                                drop-shadow-xl">
+                    { data.description}
+                </p>
+                <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+                    <button className="bg-white
+                                       text-white
+                                       bg-opacity-30
+                                       rounded-md
+                                       py-1
+                                       md:py-2
+                                       px=2 md:px-4
+                                       w-auto
+                                       text-xs
+                                       lg:text-lg
+                                       font-semibold
+                                       flex
+                                       flex-row
+                                       items-center
+                                       hover:bg-opacity-20
+                                       transition
+                                       ">
+                        <AiOutlineInfoCircle className="mr-1"/>
+                        More Info
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ frontend/components/PlayButton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BsFillPlayFill } from "react-icons/bs";
+
+interface PlayBtnProbs{
+    movieId:string
+}
+
+const PlayButton: ReactFC<PlayBtnProbs> = ({movieId}) => {
+    
+}
